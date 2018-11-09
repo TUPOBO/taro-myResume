32fe95cfc45e676ed06de007e6b473759430ef25
TUPOBO <1028461931@qq.com>
2018-11-09 16:47:41 +0800

    simply build projects page

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,11 +1,12 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Swiper, SwiperItem, Image } from '@tarojs/components'
-import { AtGrid } from 'taro-ui'
+import { AtAccordion, AtList, AtListItem } from 'taro-ui'
+import projects from '../../projects'
 import './index.css'
 
 export default class Index extends Component {
   config = {
-    navigationBarTitleText: '个人简历'
+    navigationBarTitleText: '实战项目'
   }
 
   componentWillMount() {}
@@ -24,79 +25,61 @@ export default class Index extends Component {
         <Swiper
           className='test-h'
           indicatorColor='#999'
-          indicatorActiveColor='#333'
+          indicatorActiveColor='#f56'
           circular
           indicatorDots
           autoplay
+          style='height: 180px;'
         >
           <SwiperItem>
-            <View className='demo-text-1'>
-              <Image
-                style='width: 700px;height: 300px;background: #fff;'
-                src='http://placecorgi.com/700/300'
-              />
-            </View>
+            <Image
+              style='height: 180px;width:400px;background: #fff;'
+              src='../../images/banner/Vanilla-JS.png'
+            />
           </SwiperItem>
           <SwiperItem>
             <View className='demo-text-2'>
               <Image
-                style='width: 700px;height: 300px;background: #fff;'
-                src='http://placecorgi.com/700/300'
+                style='height: 180px;width:400px;background: #fff;'
+                src='../../images/banner/react.png'
               />
             </View>
           </SwiperItem>
           <SwiperItem>
             <View className='demo-text-3'>
               <Image
-                style='width: 700px;height: 300px;background: #fff;'
-                src='http://placecorgi.com/700/300'
+                style='height: 180px;width:400px;background: #fff;'
+                src='../../images/banner/vue.png'
               />
             </View>
           </SwiperItem>
           <SwiperItem>
             <View className='demo-text-4'>
               <Image
-                style='width: 700px;height: 300px;background: #fff;'
-                src='http://placecorgi.com/700/300'
+                style='height: 180px;width:400px;background: #fff;'
+                src='../../images/banner/xcx.jpg'
               />
             </View>
           </SwiperItem>
         </Swiper>
-        <AtGrid
-          mode='rect'
-          data={[
-            {
-              image:
-                'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png',
-              value: '领取中心'
-            },
-            {
-              image:
-                'https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png',
-              value: '找折扣'
-            },
-            {
-              image:
-                'https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png',
-              value: '领会员'
-            },
-            {
-              image:
-                'https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png',
-              value: '新品首发'
-            },
-            {
-              image:
-                'https://img14.360buyimg.com/jdphoto/s72x72_jfs/t17251/336/1311038817/3177/72595a07/5ac44618Na1db7b09.png',
-              value: '领京豆'
-            },
-            {
-              image:
-                'https://img30.360buyimg.com/jdphoto/s72x72_jfs/t5770/97/5184449507/2423/294d5f95/595c3b4dNbc6bc95d.png',
-              value: '手机馆'
-            }
-          ]}
-        />
+        {projects.map(item => (
+          <AtAccordion open key={item.title} title={item.title}>
+            <AtList>
+              {item.projects.map(project => (
+                <AtListItem
+                  key={project.id}
+                  title={project.title}
+                  arrow='right'
+                  iconInfo={{
+                    size: 25,
+                    color: '#78A4FA',
+                    value: 'calendar'
+                  }}
+                />
+              ))}
+            </AtList>
+          </AtAccordion>
+        ))}
       </View>
     )
   }
---
+++ src/projects.js
@@ -0,0 +1,64 @@
+const projects = [
+  {
+    title: 'JavaScript',
+    projects: [
+      {
+        title: '弹珠游戏',
+        id: 0
+      },
+      {
+        title: '青蛙跳河',
+        id: 1
+      },
+      {
+        title: '记忆游戏',
+        id: 2
+      }
+    ]
+  },
+  {
+    title: 'React',
+    projects: [
+      {
+        title: '宠物领养',
+        id: 3
+      },
+      {
+        title: '共享单车管理后台',
+        id: 4
+      },
+      {
+        title: '个人博客',
+        id: 5
+      }
+    ]
+  },
+  {
+    title: '小程序',
+    projects: [
+      {
+        title: '涂魄读书',
+        id: 6
+      },
+      {
+        title: '前端工程师简历',
+        id: 7
+      }
+    ]
+  },
+  {
+    title: 'Vue',
+    projects: [
+      {
+        title: '计算器',
+        id: 8
+      },
+      {
+        title: 'Todo',
+        id: 9
+      }
+    ]
+  }
+]
+
+export default projects
